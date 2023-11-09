ad0e1a87e659b13c8f60a856714f72cc51fd8965
rqhappyy <dtereschenko110@gmail.com>
2023-11-09 11:43:59 +0300

    task5

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,20 +2,21 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-        if(year%4==0){
+        if((year%4!=0)&&(year%400!=0)){
             switch (month) {
                 case 1, 3, 5, 8, 10, 12 -> System.out.println("31");
                 case 4, 6, 9, 11 -> System.out.println("30");
-                case 2 -> System.out.println("29");
+                case 2 -> System.out.println("28");
                 default -> System.out.println("invalid date");
             }
+
         }else if(year<0){
             System.out.println("invalid date");
         }else{
             switch (month) {
                 case 1, 3, 5, 8, 10, 12 -> System.out.println("31");
                 case 4, 6, 9, 11 -> System.out.println("30");
-                case 2 -> System.out.println("28");
+                case 2 -> System.out.println("29");
                 default -> System.out.println("invalid date");
             }
         }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -3,10 +3,10 @@ package school.mjc.stage0.conditions.finalTask;
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
         int result = dividend/divider;
-        if(result*divider==dividend){
-            System.out.println("can be divided completely");
-        }else if(result*divider==0){
+        if(result*divider==0){
             System.out.println("division by zero");
+        }else if(result*divider==dividend){
+            System.out.println("can be divided completely");
         }else{
             System.out.println("cannot be divided completely");
         }
